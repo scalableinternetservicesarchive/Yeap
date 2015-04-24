6158d47d2f639c02d14e7b915c01eca0ed9e15fc
Li Zhang <ycy828f@gmail.com>
2015-04-24 08:54:00 -0700

    Setup Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
 
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
+
+  resources :users
 end
