a86eb0f16db471af4895b1d544f7d7106470ccdb
Li Zhang <ycy828f@gmail.com>
2015-04-24 09:48:58 -0700

    Add update delete and admin to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424161241) do
+ActiveRecord::Schema.define(version: 20150424164031) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150424161241) do
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
     t.string   "remember_digest", limit: 255
+    t.boolean  "admin",           limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
