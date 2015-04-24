6ebedcb30211c6895b943c8cc3b69608ba0e1864
Li Zhang <ycy828f@gmail.com>
2015-04-24 09:25:18 -0700

    Add session and cookies

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 end
