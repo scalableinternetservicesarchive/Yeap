ca1dde9ce1ced214302c687cde8ffbd6da09d051
zandersun <zandersun@gmail.com>
2015-04-24 22:15:32 -0700

    seeded data with 50 locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+path = 'db/seeds/data.json'
+
+# must modify the current 'data.json' by adding objects in one single object
+places = ActiveSupport::JSON.decode(File.read(path))
+#puts places["title"]
+
+place_id = 0
+categories = {}
+
+# add each place
+places.each do |place|
+  place_id = place_id + 1
+  Location.create!(:name => place['name'], :address => place['address'], :img_url => place['img_url'], :intro => place['intro'], :rate => place['rate'])
+
+end
+puts place_id.to_s + ' locations has been added into Place'
+
