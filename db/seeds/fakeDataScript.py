4469762b31c2c5336639afd5ad565af1ad21f419
zandersun <zandersun@gmail.com>
2015-06-05 02:05:28 +0800

    100000 fake locations generated

---
+++ db/seeds/fakeDataScript.py
@@ -0,0 +1,32 @@
+import json
+import re
+import sys
+import os
+import shutil
+import random
+from random import randint
+import random, string
+from random import randint
+def randomword(length):
+   return ''.join(random.choice(string.lowercase) for i in range(length))
+
+ofile = open('faked_data.json','w')
+json_array = []
+records = set([])
+
+for i in xrange(110000):
+    tmp = {}
+    tmp["name"] = randomword(15)
+    while tmp["name"] in records:
+      tmp["name"] = randomword(15)   
+      print "duplicate "+str(i)
+    tmp["address"] = "#"+str(i)+ " random street, "  + randomword(5)
+    tmp["rate"] = "5.0"
+    tmp["intro"] = "random generated " + randomword(10)
+    tmp["avg_price"] = "$$"
+    tmp["img_name"] = "default.jpg"
+    records.add(tmp["name"])
+    json_array.append(tmp)
+        
+json.dump(json_array,ofile,indent = 4)
+ofile.close()
